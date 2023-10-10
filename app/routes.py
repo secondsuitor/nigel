6f736d507324cdccc1b83d079daf2986883e5f9d
secondsuitor <pete@secondsuitor.com>
2023-10-09 19:00:23 -0700

    move routes to own module

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from flask import render_template, request
+from models import db, Post
+
+def home():
+    posts = Post.query.all()
+    return render_template('home.html', posts=posts)
+
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', post=post)
+
+def new_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        post = Post(title=title, content=content)
+        db.session.add(post)
+        db.session.commit()
+    return render_template('new_post.html')
