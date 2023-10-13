68c748cdcb2ea09f6e9c1b39a75c32367e2d9a60
secondsuitor <pete@secondsuitor.com>
2023-10-12 18:36:50 -0700

    add fake 'posts'

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,10 @@ from app.models import db, Post
 
 def home():
     #posts = Post.query.all()
+    # place holder for real posts
+    posts = ['Hi this is fake', 'This is also fake', 'This is fake too']
     title = "Home"
-    return render_template('home.html', title=title) #, posts=posts)
+    return render_template('home.html', title=title, posts=posts)
 
 def post(id):
     post = Post.query.get_or_404(id)
