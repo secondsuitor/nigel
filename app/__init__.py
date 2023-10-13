74bfda5c6b506916f871625c52abc7f900356ea7
secondsuitor <pete@secondsuitor.com>
2023-10-12 20:01:39 -0700

    add some db stuff per chat

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from app.models import db
 from app.routes import home, post, new_post
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db.init_app(app)
+
+migrate = Migrate(app, db)
+
+app.cli.add_command(MigrateCommand)
 
 app.route('/')(home)
 app.route('/post/<int:id>')(post)
