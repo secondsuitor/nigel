8ff925415c00d74229209b2b7c6f67976a5eede1
secondsuitor <pete@secondsuitor.com>
2023-10-28 12:20:36 -0700

    add basic user table

--- app/models.py
+++ app/models.py
@@ -4,3 +4,9 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    logged_in = db.Column(db.Boolean, default=False)
