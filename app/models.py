0a0bf7245f6d12332eaa230f02c68bf16b412c9c
secondsuitor <pete@secondsuitor.com>
2023-10-28 20:57:01 -0700

    password hashing

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,6 +10,12 @@ class Post(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     logged_in = db.Column(db.Boolean, default=False)
     email = db.Column(db.String(100), nullable=False)
+
+    def generate_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
