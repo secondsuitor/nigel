84a6b51824c9841121388ffc1cae45ca0d67cfe0
secondsuitor <pete@secondsuitor.com>
2023-10-29 18:53:54 -0700

    add a secret key

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SECRET_KEY'] = 'your-secret-key-goes-here'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
